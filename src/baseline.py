b7e0a40c3882e3ff476255a87f9afd4b77f28f33
MiguelAFH <miguel.angel.fh@hotmail.com>
2024-05-15 21:48:32 -0700

    Fixed baseline

--- src/baseline.py
+++ src/baseline.py
@@ -14,8 +14,10 @@ def get_y(item):
 
 def fine_tune(dls, model, epochs=4, export_path=None, tb_logs_path=None):
     learn = vision_learner(dls, model, metrics=accuracy_multi, cbs=[TensorBoardCallback(log_dir=tb_logs_path)])
-    learn.model.to('mps')
-    # exit()
+    if torch.cuda.is_available():
+        learn.model.to('cuda')
+    elif torch.backends.mps.is_available():
+        learn.model.to('mps')
     learn.fine_tune(epochs)
     if export_path:
         learn.export(export_path)
@@ -60,18 +62,7 @@ if __name__ == "__main__":
       item_tfms=[Resize(224, method="squish")]
     ).dataloaders(train_data)
     
-    # learn1 = fine_tune(dls, densenet121, 5, os.path.join(MODELS_PATH,"densenet121.pkl"), tb_logs_path=os.path.join(TB_LOGS_PATH, "densenet121"))
+    learn1 = fine_tune(dls, densenet121, 5, os.path.join(MODELS_PATH,"densenet121.pkl"), tb_logs_path=os.path.join(TB_LOGS_PATH, "densenet121"))
     learn2 = fine_tune(dls, alexnet, 5, os.path.join(MODELS_PATH, "alexnet.pkl"), tb_logs_path=os.path.join(TB_LOGS_PATH, "alexnet"))
-    learn2 = fine_tune(dls, resnet50, 5, os.path.join(MODELS_PATH, "resnet50.pkl"), tb_logs_path=os.path.join(TB_LOGS_PATH, "resnet50"))
-    learn3 = fine_tune(dls, vgg19, 5, os.path.join(MODELS_PATH, "vgg19.pkl"), tb_logs_path=os.path.join(TB_LOGS_PATH, "vgg19"))
-
-    #Run validation and create accuracy barchart for each model
-    # validate(learn1, test_data)
-    # validate(learn2, test_data)
-    # validate(learn3, test_data)
-    #Create accuracy bar chart with all models
-    # accs = [0.75, 0.85, 0.80]
-    # models = ["resnet18", "resnet50", "vgg19"]
-    # plt.bar(models, accs)
-    
-    
+    learn3 = fine_tune(dls, resnet50, 5, os.path.join(MODELS_PATH, "resnet50.pkl"), tb_logs_path=os.path.join(TB_LOGS_PATH, "resnet50"))
+    learn4 = fine_tune(dls, vgg19, 5, os.path.join(MODELS_PATH, "vgg19.pkl"), tb_logs_path=os.path.join(TB_LOGS_PATH, "vgg19"))
