32be03415e6d36d8eb6b27ed400e6c5e881ed267
MiguelAFH <miguel.angel.fh@hotmail.com>
2024-05-15 20:17:03 -0700

    Adding baselines

--- src/baseline.py
+++ src/baseline.py
@@ -1,10 +1,10 @@
 from fastcore.all import *
 from fastai.vision.all import *
+from fastai.callback.tensorboard import TensorBoardCallback
 import time
 import pandas as pd
 from datasets import load_dataset, Image
 import os
-
 
 def get_x(item):
     return item['image']['path']
@@ -12,9 +12,9 @@ def get_x(item):
 def get_y(item):
     return item['labels']
 
-def fine_tune(dls, model, epochs=4, export_path=None):
-    learn = vision_learner(dls, model, metrics=accuracy_multi)
-    learn.model.to('cuda')
+def fine_tune(dls, model, epochs=4, export_path=None, tb_logs_path=None):
+    learn = vision_learner(dls, model, metrics=accuracy_multi, cbs=[TensorBoardCallback(log_dir=tb_logs_path)])
+    learn.model.to('mps')
     # exit()
     learn.fine_tune(epochs)
     if export_path:
@@ -34,14 +34,14 @@ def dataset_stats(data, name):
     print(data.dtypes)
     print("\nMissing Values:")
     print(data.isnull().sum())
-    # print("\nValue Counts for each column:")
-    # for col in data.columns:
-    #     print("\n", col)
-    #     print(data[col].value_counts())
     print("="*50 + "\n")
 
 if __name__ == "__main__":
-    MODELS_PATH = "src/models"
+    MODELS_PATH = "models"
+    TB_LOGS_PATH = "tb_logs"
+    os.makedirs(MODELS_PATH, exist_ok=True)
+    os.makedirs(TB_LOGS_PATH, exist_ok=True)
+    
     
     ds_name = "alkzar90/NIH-Chest-X-ray-dataset"
     dataset_with_image_data = load_dataset(ds_name, 'image-classification', data_dir='./data')
@@ -60,19 +60,18 @@ if __name__ == "__main__":
       item_tfms=[Resize(224, method="squish")]
     ).dataloaders(train_data)
     
-    dls.show_batch(max_n=10)
-
-    learn1 = fine_tune(dls, resnet18, 8, os.path.join(MODELS_PATH,"resnet18.pkl"))
-    # learn2 = fine_tune(dls, resnet50, 4, os.path.join(MODELS_PATH, "resnet50.pkl"))
-    # learn3 = fine_tune(dls, vgg19, 6, os.path.join(MODELS_PATH, "vgg19.pkl"))
+    # learn1 = fine_tune(dls, densenet121, 5, os.path.join(MODELS_PATH,"densenet121.pkl"), tb_logs_path=os.path.join(TB_LOGS_PATH, "densenet121"))
+    learn2 = fine_tune(dls, alexnet, 5, os.path.join(MODELS_PATH, "alexnet.pkl"), tb_logs_path=os.path.join(TB_LOGS_PATH, "alexnet"))
+    learn2 = fine_tune(dls, resnet50, 5, os.path.join(MODELS_PATH, "resnet50.pkl"), tb_logs_path=os.path.join(TB_LOGS_PATH, "resnet50"))
+    learn3 = fine_tune(dls, vgg19, 5, os.path.join(MODELS_PATH, "vgg19.pkl"), tb_logs_path=os.path.join(TB_LOGS_PATH, "vgg19"))
 
     #Run validation and create accuracy barchart for each model
-    validate(learn1, test_data)
+    # validate(learn1, test_data)
     # validate(learn2, test_data)
     # validate(learn3, test_data)
     #Create accuracy bar chart with all models
-    accs = [0.75, 0.85, 0.80]
-    models = ["resnet18", "resnet50", "vgg19"]
-    plt.bar(models, accs)
+    # accs = [0.75, 0.85, 0.80]
+    # models = ["resnet18", "resnet50", "vgg19"]
+    # plt.bar(models, accs)
     
     
